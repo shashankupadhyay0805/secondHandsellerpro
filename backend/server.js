57b8cf0349ed339406eeb4c9e623d2c4f04a67e2
Shashank Upadhyay <147583703+shashankupadhyay0805@users.noreply.github.com>
2025-12-11 23:19:24 +0530

    Uncomment server listening code

--- backend/server.js
+++ backend/server.js
@@ -56,8 +56,8 @@ app.use((err, req, res, next) => {
   })
 })
 
-// const PORT = process.env.PORT || 5000
-// app.listen(PORT, console.log(`Server is running on port ${PORT}`))
+const PORT = process.env.PORT || 5000
+app.listen(PORT, console.log(`Server is running on port ${PORT}`))
 export default app;
 
 
